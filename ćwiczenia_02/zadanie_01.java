a6abe96c255fada8a49e6ec7576168efceb62bca
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-10-10 13:38:55 +0200

    Create zadanie_01.java

---
+++ ćwiczenia_02/zadanie_01.java
@@ -0,0 +1,30 @@
+// Napisz program, który tworzy listę tablicową 10 liczb
+// całkowitych, a następnie wyświetla je w konsoli w porządku
+// odwrotnym do wprowadzenia.
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> listaLiczbCalkowitych = new ArrayList<>();
+        listaLiczbCalkowitych.add(3);
+        listaLiczbCalkowitych.add(4);
+        listaLiczbCalkowitych.add(5);
+        listaLiczbCalkowitych.add(6);
+        listaLiczbCalkowitych.add(7);
+        listaLiczbCalkowitych.add(8);
+        listaLiczbCalkowitych.add(9);
+        listaLiczbCalkowitych.add(10);
+        listaLiczbCalkowitych.add(11);
+        listaLiczbCalkowitych.add(12);
+        
+        for (Integer liczbCalkowitych : listaLiczbCalkowitych) {
+            System.out.println(liczbCalkowitych);
+        }
+
+        /* for (int i=0; i < listaLiczbCalkowitych.size(); i++){
+            System.out.println(listaLiczbCalkowitych.get(i));
+        } */
+    }
+}

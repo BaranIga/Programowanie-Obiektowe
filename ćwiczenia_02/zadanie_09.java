3987e53929a2d29f71f3dff53f11753d7013b4ba
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-10-10 15:21:50 +0200

    Create zadanie_09.java

---
+++ ćwiczenia_02/zadanie_09.java
@@ -0,0 +1,10 @@
+/* Napisz program, który przyjmuje jako wejście pojedynczy znak oraz liczbę całkowitą n. Używając klasy 
+  StringBuilder, zbuduj i wypisz piramidę o wysokości n, gdzie każdy poziom piramidy składa się z podanego 
+  znaku. Na przykład dla znaku * i n=3, oczekiwany wynik to:
+                                                              *
+                                                             ***
+                                                            *****
+*/
+
+
+// photo 15:19

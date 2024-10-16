1c96b852f35e3c5510f124df3cfbb144a808f94f
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-10-16 18:50:49 +0200

    Update zadanie_08.java

--- ćwiczenia_02/zadanie_08.java
+++ ćwiczenia_02/zadanie_08.java
@@ -17,10 +17,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> liczby = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            liczby.add(i);
-        }
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Wprowadż tekst do odwócenia: ");

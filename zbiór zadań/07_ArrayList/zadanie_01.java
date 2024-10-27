2b44f2b994704832ba5ed72e2df109435c9be015
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-10-27 18:43:45 +0100

    Update zadanie_01.java

--- zbiór zadań/07_ArrayList/zadanie_01.java
+++ zbiór zadań/07_ArrayList/zadanie_01.java
@@ -1,1 +1,17 @@
-   
+// Napisz program, który tworzy listę tablicową 10 liczb całkowitych, a następnie wyświetla
+// je w konsoli w porządku odwrotnym do wprowadzenia.
+
+import java.util.ArrayList;
+
+public class ReverseArrayList {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i=0; i<10; i++){
+            list.add(i);
+        }
+
+        System.out.println(list.reversed());
+    }
+
+}

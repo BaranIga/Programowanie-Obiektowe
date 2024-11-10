9694f6486b648cd504a71aafb42db355b56958b0
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-11-10 11:46:00 +0100

    Create zadanie_06.java

---
+++ zbiór zadań/09_pojęcie_klasy_obiektu/zadanie_06.java
@@ -0,0 +1,21 @@
+// KLASA Car
+
+public class Car {
+
+    public String brand;
+
+}
+
+// KLASA TestCar
+
+public class TestCar {
+
+    public static void main(String[] args) {
+        Car myCar = null;
+
+        if (myCar != null) {
+            System.out.println(myCar.brand);
+        }
+
+    }
+}

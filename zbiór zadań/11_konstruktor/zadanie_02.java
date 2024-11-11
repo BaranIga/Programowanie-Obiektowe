fc0562b5fdb2aa6d180819f2f0d9779fe4ec1ac5
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-11-11 15:49:36 +0100

    Create zadanie_02.java

---
+++ zbiór zadań/11_konstruktor/zadanie_02.java
@@ -0,0 +1,16 @@
+// Zaprojektuj klasę Person, która będzie zawierać dwa pola: firstName, lastName. 
+// Zaimplementuj konstruktor, który przyjmuje dwa
+// argumenty odpowiadające polom klasy. Nazwy parametrów muszą być takie same jak nazwy pól.
+
+
+public class Person {
+
+    public String firstName;
+    public String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}

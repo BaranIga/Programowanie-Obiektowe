f098ba60a9a27f13cfc293918a697e05893fbe63
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-11-28 13:55:10 +0100

    Create Main.java

---
+++ REKORDY/zadanie_01/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BookDTO b1 = new BookDTO("Harry Potter", "J.K. Rowling", 35.50, 2004);
+        BookDTO b2 = new BookDTO("Harry Potter", "J.K. Rowling", 35.50, 2004);
+        BookDTO b3 = new BookDTO("Harry Potter", "J.K. Rowling", 35.50, 2004);
+        System.out.println(b1);
+    }
+}

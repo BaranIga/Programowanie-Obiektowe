2d6bbd4b95573e213d4577657ea60762e8a31280
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-11-30 18:06:39 +0100

    Create Main.java

---
+++ REKORDY/zadanie_02/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Agnieszka", "Kot",  new Address("Śliwkowa", 12, 123456, "Warszawa"));
+        System.out.println(person);
+    }
+}

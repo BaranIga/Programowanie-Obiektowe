4995c6e27a31edf8e7789b693a7aad6b42e2f6ab
BaranIga <153220409+BaranIga@users.noreply.github.com>
2025-01-09 15:05:24 +0100

    Create Counter.java

---
+++ PROGRAMOWANIE_GENERYCZNE/zadanie_02/Counter.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Counter <T> {
+    int count;
+    ArrayList<T> list = new ArrayList<>();
+
+    public void add(T element) {
+        list.add(element);
+        this.count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
